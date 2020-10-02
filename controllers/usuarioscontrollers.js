58d9823ac7c26ce4a2ea6b421587f173a1596c62
jorge abascal lopez <jorgeabascal20@gmail.com>
2020-10-01 22:21:54 -0500

    Agregando usuarios a la base de datos

--- controllers/usuarioscontrollers.js
+++ controllers/usuarioscontrollers.js
@@ -1,4 +1,25 @@
+const Usuario = require("../models/Usuario");
 
-exports.crearUsuario= (req, res) =>{
-    console.log(req.body);
+exports.crearUsuario= async (req, res) =>{
+    //Agregando usuariops a la base de datoss mongo db
+
+    //se recomientda agregar un try por cualquier error
+    try {
+        let usuario;
+         //Crea el nuevo usuario
+         usuario = new Usuario(req.body);
+
+         //Guarda usuario 
+         await usuario.save();
+
+         //mensaje de confirmacion
+         res.send("Usuario creado correctamente");
+
+    } catch (error) {
+        
+    }
+
+
+
+    //Finalizando creacion de usuarios
 }
