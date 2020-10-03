987444f6826afe206c29eb5a0c9110d4df308b8e
jorge abascal lopez <jorgeabascal20@gmail.com>
2020-10-03 00:04:16 -0500

    Terminando api de usuarios

--- controllers/usuarioscontrollers.js
+++ controllers/usuarioscontrollers.js
@@ -1,22 +1,60 @@
 const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const {validationResult} = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 exports.crearUsuario= async (req, res) =>{
+
+     //Aplicando el express-validation que realizamos en el routes
+     const validacion = validationResult(req);
+     if(!validacion.isEmpty()){
+         return res.status(400).json({validacion: validacion.array()})
+     }
+
+    //Aplicando destructuring al objeto req para extraer el correo y contraseña ingresada
+    const {email, password} = req.body;
+
     //Agregando usuariops a la base de datoss mongo db
 
     //se recomientda agregar un try por cualquier error
     try {
-        let usuario;
+
+        let usuario= await Usuario.findOne({email});
+
+        if(usuario){
+            return res.status(400).json({msg: "El usuario ya existe"});
+        }
          //Crea el nuevo usuario
          usuario = new Usuario(req.body);
+
+         //Hashear el password
+         const salt = await bcryptjs.genSalt(10);
+         usuario.password = await bcryptjs.hash(password, salt);
 
          //Guarda usuario 
          await usuario.save();
 
+         //Crear y firmar el jwt
+         const payload = {
+             usuario: {
+                 id: usuario.id
+             }
+         }
+
+         //Firmar el jwt
+         jwt.sign(payload, process.env.SECRETA,{
+            expiresIn: 3600000
+         } ,(error, token) => {
+            if(error)throw error;
+            res.json({token});
+         });
+
          //mensaje de confirmacion
-         res.send("Usuario creado correctamente");
+        // res.json({msg: "El usuario se creo con exito"});
 
     } catch (error) {
-        
+        console.log(error);
+        res.status(400).send("hubo un error");
     }
 
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,10 +2,16 @@
 const express = require("express");
 const router = express.Router();
 const usuariocontrollers = require("../controllers/usuarioscontrollers");
+const {check} = require("express-validator");
 
 //Crea un usuario
 //api/usuarios
-router.post("/", 
+router.post("/",  
+   [
+       check("nombre", "El nombre es obligatorio").not().isEmpty(),
+       check("email", "Ingrese un correo valido").isEmail(),
+       check("password", "La contraseña debe tener 6 digitos minimos").isLength({min: 6})
+   ],
     usuariocontrollers.crearUsuario
 )
 
